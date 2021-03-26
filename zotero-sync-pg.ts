392f85bd733d20462cd1d36a7d1b655e0fecfb24
Emiliano Heyns <retorquere@users.noreply.github.com>
2021-03-26 15:04:58 +0100

    Update zotero-sync-pg.ts

--- zotero-sync-pg.ts
+++ zotero-sync-pg.ts
@@ -1,5 +1,16 @@
 #!node_modules/.bin/ts-node
 
+/*
+Sync a zotero library/groups to a Pg database for analysis
+Copyright (C) <year> <name of author>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // tslint:disable no-console
 
 import request = require('request-promise')

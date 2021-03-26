8d1f8dfe7933ab53aff9ade9da1a3ae141a983be
Emiliano Heyns <retorquere@users.noreply.github.com>
2021-03-26 15:14:10 +0100

    Update zotero-sync-pg.ts

--- zotero-sync-pg.ts
+++ zotero-sync-pg.ts
@@ -2,7 +2,7 @@
 
 /*
 Sync a zotero library/groups to a Pg database for analysis
-Copyright (C) <year> <name of author>
+Copyright (C) 2021 Richard Kaplan
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
